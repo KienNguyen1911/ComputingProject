ee332e5f7d34cf97f4da3708b3e130eacb12a81d
KienNguyen1911 <ngkien1911@gmail.com>
2022-10-14 10:23:40 +0700

    edit public | edit welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,7 +56,7 @@
     </nav>
     {{-- Slider --}}
     <div class="slider">
-      <img src="{{asset('image/main-pic-1.jpg')}}" alt="Main">
+      <img src="{{asset('img/main-pic-1.jpg')}}" alt="Main">
     </div>
     <div class="img-text">
       <div class="img-title">
@@ -109,6 +109,10 @@
         </div>
       </div>
     </div>
+
+    @include('components.content')
+    {{-- Footer --}}
+
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
