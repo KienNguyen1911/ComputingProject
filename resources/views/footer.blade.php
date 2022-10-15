083e8fea8e8c2629ab885a9eded6fab975456b6b
PhamLonq <longpnbhaf200132@fpt.edu.vn>
2022-10-15 10:17:32 +0700

    add gosw

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,20 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">   <!--bootstrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"><!--bootstrap-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">    <!--fontonlin-->
+    <link rel="preconnect" href="https://fonts.googleapis.com"><!--fontonlin-->
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../css/footer.css"> 
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     <title>document</title>
 </head>
 <body>
+<!--------------------- gosw ------------->
+<div class="gosw">
+        <div class="gosw-container">
+            <div class="gosw-title">
+                <h2>Go somewhere</h2>
+            </div>
+            <div class="gosw-subtitle">
+                <p>Lets's go on an adventure</p>
+            </div>
+            <div class="goswn-nav">
+                <button class="ft-btn"><a class="nav-item" href="">Feature</a></button>
+                <a class="nav-item" href="">Family-friendly</a>
+                <a class="nav-item" href="">On sale</a>
+                <a class="nav-item" href="">Sub nav</a>
+            </div>
+            <div class="gosw-menu row">
+                <div class="card  Col-lg-3 ">
+                    <div class="card-img">
+                        <a href=""><img style="border-top-right-radius: 24px;border-top-left-radius: 24px;"
+                            src="https://ui8-fleet-html.herokuapp.com/img/content/catalog-pic-1@2x.jpg" alt="logo"
+                            class="card-logo"></a>
+                    </div>
+                    <div class="card-body">
+                        <div class="card-des">
+                            <div class="card-name">
+                                <a href="" style="color:black;"><p>the grand resort</p></a>
+                            </div>
+                            <div class="card-price" style="">
+                                <a href=""style="color:black;" >$267</a>
+                            </div>
+                        </div>
+                        <div class="line"></div>
+                        <div class="card-day">
+                            <p>Tue, Jul 20 - Fri, Jul 23</p>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+<!------------------------------footer -->
     <div class="footer">
         <div class="container">
             <div class="row khungtrong">
