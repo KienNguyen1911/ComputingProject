37972c802609f63e958f607f14e7094d3890a3a0
Hiu Hoang <89955228+HiuHoang@users.noreply.github.com>
2022-10-15 10:44:26 +0700

    change test to header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -9,7 +9,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap" rel="stylesheet">
         <link href="{{asset('css/base.css')}}" rel="stylesheet">  
-        <link rel="stylesheet" href="{{asset('css/test.css')}}">
+        <link rel="stylesheet" href="{{asset('css/header.css')}}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     </head>
     <body>
@@ -56,81 +56,81 @@
             <div class="background">
                 <div class="background-image">
                     <img src="{{asset('image/main-pic-1.jpg')}}" style="border-radius: 25px"alt="Main">
+                    <div class="background-text">
+                        <div class="background-text__title">
+                            Air, sleep, dream
+                        </div>
+                        <div class="background-text__subtitle">
+                            Find and book a great experience.
+                        </div>
+                        <div class="background-text__button">
+                            Start your search
+                        </div>
+                    </div>
                 </div>
-                <div class="background-text">
-                    <div class="background-text__title">
-                        Air, sleep, dream
+                <div class="search">
+                    <div class="search-navbar">
+                        <ul class="nav nav-tabs">
+                            <li class="nav-item">
+                              <a class="nav-link active" aria-current="page" href="#">Stays</a>
+                              <li><hr class="dropdown-divider"></li>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="#">Flights</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="#">Cars</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="#">Things to do</a>
+                            </li>
+                          </ul>
                     </div>
-                    <div class="background-text__subtitle">
-                        Find and book a great experience.
-                    </div>
-                    <div class="background-text__button">
-                        Start your search
+                    <div class="search-content">
+                        <div class="search-bar">
+                            <div class="search-bar__icon">
+                                <i class="fa-solid fa-location-dot"></i>
+                            </div>
+                            <div class="search-bar__text">
+                                Location
+                                <div class="search-bar__subtext">
+                                Where are you going?
+                                </div>
+                            </div>
+                        </div>
+                        <div class="search-bar">
+                            <div class="search-bar__icon">
+                                <i class="fa-solid fa-calendar-days"></i>
+                            </div>
+                            <div class="search-bar__text">
+                                Check in - out
+                                <div class="search-bar__subtext">
+                                Add date
+                                </div>
+                            </div>
+                        </div>
+                        <div class="search-bar">
+                            <div class="search-bar__icon">
+                                <i class="fa-solid fa-user-pen"></i>
+                            </div>
+                            <div class="search-bar__text">
+                                Travelers
+                                <div class="search-bar__subtext">
+                                    Travelers
+                                </div>
+                            </div>
+                        </div>
+                        <div class="search-bar">
+                            <div class="search-bar__icon">
+                                <div class="search-bar__icon__background">
+                                    <i class="fa-solid fa-magnifying-glass" style="margin-left: 22.5px; margin-top: 22.5px"></i>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         {{--Search--}}
-            <div class="search">
-                <div class="search-navbar">
-                    <ul class="nav nav-tabs">
-                        <li class="nav-item">
-                          <a class="nav-link active" aria-current="page" href="#">Stays</a>
-                          <li><hr class="dropdown-divider"></li>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">Flights</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">Cars</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">Things to do</a>
-                        </li>
-                      </ul>
-                </div>
-                <div class="search-content">
-                    <div class="search-bar">
-                        <div class="search-bar__icon">
-                            <i class="fa-solid fa-location-dot"></i>
-                        </div>
-                        <div class="search-bar__text">
-                            Location
-                            <div class="search-bar__subtext">
-                            Where are you going?
-                            </div>
-                        </div>
-                    </div>
-                    <div class="search-bar">
-                        <div class="search-bar__icon">
-                            <i class="fa-solid fa-calendar-days"></i>
-                        </div>
-                        <div class="search-bar__text">
-                            Check in - out
-                            <div class="search-bar__subtext">
-                            Add date
-                            </div>
-                        </div>
-                    </div>
-                    <div class="search-bar">
-                        <div class="search-bar__icon">
-                            <i class="fa-solid fa-user-pen"></i>
-                        </div>
-                        <div class="search-bar__text">
-                            Travelers
-                            <div class="search-bar__subtext">
-                                Travelers
-                            </div>
-                        </div>
-                    </div>
-                    <div class="search-bar">
-                        <div class="search-bar__icon">
-                            <div class="search-bar__icon__background">
-                                <i class="fa-solid fa-magnifying-glass" style="margin-left: 22.5px; margin-top: 22.5px"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     </body>
