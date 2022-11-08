73117e712b363f779c32beb1e1d46022218943c7
KienNguyen1911 <ngkien1911@gmail.com>
2022-11-08 16:54:07 +0700

    Update home detail

--- resources/views/home-details.blade.php
+++ resources/views/home-details.blade.php
@@ -1,8 +1,51 @@
+<link rel="stylesheet" href="{{ asset('css/header.css') }}">
 @include('components.head')
+{{-- @include('components.header') --}}
+
 
 <link rel="stylesheet" href="{{ asset('css/home-details.css') }}">
 
 <body>
+    {{-- header in header --}}
+    <div class="header">
+        <div class="navbar">
+            <div class="navbar-left">
+                <div class="navbar-logo">
+                    <a href="index.html">
+                        <img class="some-icon" src="https://ui8-fleet-html.herokuapp.com/img/logo-dark.svg" alt="Fleet" data-metatip="true" data-allytip="true" data-selected="true" data-label-id="0">
+                    </a>
+                </div>
+                <div class="navbar-feature">
+                    <div class="navbar-feature__text">
+                        Traverlers
+                    </div>
+                    <div class="navbar-feature__icon">
+                        <i class="fa-sharp fa-solid fa-caret-down"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="navbar-right">
+                <a class="navbar-support" href="#">
+                    Support
+                </a>
+                <a class="navbar-button" href="">
+                    List your property
+                </a>
+                <div class="navbar-notification">
+                    <div class="navbar-icon__center">
+                        <i class="fa-regular fa-bell" style="font-size: 20px"></i>
+                    </div>
+                </div>
+                <div class="navbar-profile">
+                    <div class="navbar-icon__center">
+                        <i class="fa-solid fa-circle-user"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
     <div class="section product">
         <div class="product-center center">
             <div class="control">
