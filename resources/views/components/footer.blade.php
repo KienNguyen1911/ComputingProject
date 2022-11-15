1b2e88e19823bb00983eeb6c52ca28e0532af191
PhamLonq <longpnbhaf200132@fpt.edu.vn>
2022-11-15 16:50:01 +0700

    rename class

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,98 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"><!--bootstrap-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> -->
     <link rel="preconnect" href="https://fonts.googleapis.com"><!--fontonlin-->
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-    <!-- <link rel="stylesheet" href="../css/footer.css"> -->
-    <link rel="stylesheet" href="{{asset('css/footer.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-    <title>document</title>
+    <link rel="stylesheet" href="{{asset('css/footer.css')}}">
+
+    <title>footer</title>
 </head>
+
 <body>
-<!--------------------- gosw ------------->
-<div class="gosw">
-        <div class="gosw-container">
-            <div class="gosw-title">
-                <h2>Go somewhere</h2>
-            </div>
-            <div class="gosw-subtitle">
-                <p>Lets's go on an adventure</p>
-            </div>
-            <div class="goswn-nav">
-                <button class="ft-btn"><a class="nav-item" href="">Feature</a></button>
-                <a class="nav-item" href="">Family-friendly</a>
-                <a class="nav-item" href="">On sale</a>
-                <a class="nav-item" href="">Sub nav</a>
-            </div>
-            <div class="gosw-menu row">
-                <div class="card  Col-lg-3 ">
-                    <div class="card-img">
-                        <a href=""><img style="border-top-right-radius: 24px;border-top-left-radius: 24px;"
-                            src="https://ui8-fleet-html.herokuapp.com/img/content/catalog-pic-1@2x.jpg" alt="logo"
-                            class="card-logo"></a>
-                    </div>
-                    <div class="card-body">
-                        <div class="card-des">
-                            <div class="card-name">
-                                <a href="" style="color:black;"><p>the grand resort</p></a>
-                            </div>
-                            <div class="card-price" style="">
-                                <a href=""style="color:black;" >$267</a>
-                            </div>
-                        </div>
-                        <div class="line"></div>
-                        <div class="card-day">
-                            <p>Tue, Jul 20 - Fri, Jul 23</p>
-                        </div>
-                    </div>
-                </div>
-
-
-            </div>
-
-            <div class="gosw-menu row">
-                <div class="card  Col-lg-3 ">
-                    <div class="card-img">
-                        <a href=""><img style="border-top-right-radius: 24px;border-top-left-radius: 24px;"
-                            src="https://ui8-fleet-html.herokuapp.com/img/content/catalog-pic-1@2x.jpg" alt="logo"
-                            class="card-logo"></a>
-                    </div>
-                    <div class="card-body">
-                        <div class="card-des">
-                            <div class="card-name">
-                                <a href="" style="color:black;"><p>the grand resort</p></a>
-                            </div>
-                            <div class="card-price" style="">
-                                <a href=""style="color:black;" >$267</a>
-                            </div>
-                        </div>
-                        <div class="line"></div>
-                        <div class="card-day">
-                            <p>Tue, Jul 20 - Fri, Jul 23</p>
-                        </div>
-                    </div>
-                </div>
-
-
-            </div>
-        </div>
-    </div>
-
-<!------------------------------footer -->
+  
     <div class="footer">
-        <div class="container">
-            <div class="row khungtrong">
+        <div class="footer-container">
+            <div class="row footer-body">
                 <div class="col-lg-3">
                     <div class="footer-logo">
                         <img src="https://ui8-fleet-html.herokuapp.com/img/logo-dark.svg" alt="#">
@@ -108,18 +38,22 @@
                     </div>
                 </div>
                 <div class="col-lg-3">
-                    <div class="textlua">Join our community 🔥</div>
+                    <div class="footer-motto">Join our community 🔥</div>
                     <div>
-                        <input type="text" class="form-control nutemail" placeholder="Enter your email">
-                        <svg class="nutxanh" id="arrow-right-circle-fill" xmlns="http://www.w3.org/2000/svg" width="174.449" height="174.449" viewBox="0 0 174.449 174.449">
-                            <path id="Path_1" data-name="Path 1" d="M174.449,87.225A87.225,87.225,0,1,1,87.225,0a87.225,87.225,0,0,1,87.225,87.225ZM83.367,116.076a5.458,5.458,0,0,0,7.719,7.719L123.8,91.087a5.452,5.452,0,0,0,0-7.719L91.087,50.658a5.458,5.458,0,1,0-7.719,7.719l23.405,23.4H54.515a5.452,5.452,0,1,0,0,10.9h52.257l-23.41,23.4Z" fill="#3b71fe" fill-rule="evenodd"/>
-                        </svg>                              
+                        <input type="text" class="form-control footer-email" placeholder="Enter your email">
+                        <svg class="footer-arrow" id="arrow-right-circle-fill" xmlns="http://www.w3.org/2000/svg"
+                            width="174.449" height="174.449" viewBox="0 0 174.449 174.449">
+                            <path id="Path_1" data-name="Path 1"
+                                d="M174.449,87.225A87.225,87.225,0,1,1,87.225,0a87.225,87.225,0,0,1,87.225,87.225ZM83.367,116.076a5.458,5.458,0,0,0,7.719,7.719L123.8,91.087a5.452,5.452,0,0,0,0-7.719L91.087,50.658a5.458,5.458,0,1,0-7.719,7.719l23.405,23.4H54.515a5.452,5.452,0,1,0,0,10.9h52.257l-23.41,23.4Z"
+                                fill="#3b71fe" fill-rule="evenodd" />
+                        </svg>
                     </div>
                 </div>
             </div>
-            <div class="footer-copyright"></div>
-            <div class="coppyright">Copyright © 2021 UI8 LLC. All rights reserved</div>
+            <div class="line"></div>
+            <div class="footer-coppyright">Copyright © 2021 UI8 LLC. All rights reserved</div>
         </div>
     </div>
 </body>
+
 </html>
