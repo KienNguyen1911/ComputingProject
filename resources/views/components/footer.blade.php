631c0986a727ba3805777f818681233fffee1e8f
PhamLonq <longpnbhaf200132@fpt.edu.vn>
2022-11-22 16:59:55 +0700

    ..

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -24,12 +24,12 @@
                     <div class="footer-motto">Join our community 🔥</div>
                     <div>
                         <input type="text" class="form-control footer-email" placeholder="Enter your email">
-                        <svg class="footer-arrow" id="arrow-right-circle-fill" xmlns="http://www.w3.org/2000/svg"
-                            width="174.449" height="174.449" viewBox="0 0 174.449 174.449">
-                            <path id="Path_1" data-name="Path 1"
-                                d="M174.449,87.225A87.225,87.225,0,1,1,87.225,0a87.225,87.225,0,0,1,87.225,87.225ZM83.367,116.076a5.458,5.458,0,0,0,7.719,7.719L123.8,91.087a5.452,5.452,0,0,0,0-7.719L91.087,50.658a5.458,5.458,0,1,0-7.719,7.719l23.405,23.4H54.515a5.452,5.452,0,1,0,0,10.9h52.257l-23.41,23.4Z"
-                                fill="#3b71fe" fill-rule="evenodd" />
-                        </svg>
+                       <div> <svg class="footer-arrow" id="arrow-right-circle-fill" xmlns="http://www.w3.org/2000/svg"
+                        width="174.449" height="174.449" viewBox="0 0 174.449 174.449">
+                        <path id="Path_1" data-name="Path 1"
+                            d="M174.449,87.225A87.225,87.225,0,1,1,87.225,0a87.225,87.225,0,0,1,87.225,87.225ZM83.367,116.076a5.458,5.458,0,0,0,7.719,7.719L123.8,91.087a5.452,5.452,0,0,0,0-7.719L91.087,50.658a5.458,5.458,0,1,0-7.719,7.719l23.405,23.4H54.515a5.452,5.452,0,1,0,0,10.9h52.257l-23.41,23.4Z"
+                            fill="#3b71fe" fill-rule="evenodd" />
+                    </svg></div>
                     </div>
                 </div>
             </div>
