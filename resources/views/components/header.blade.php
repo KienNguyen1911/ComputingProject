1d25a6050135744fec7e02bf90a432949d2dba59
LK <khanhnlnhbf200025@fpt.edu.vn>
2022-12-06 17:03:43 +0700

    LK update dashboard

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -21,7 +21,8 @@
                                 Traverlers
                             </div>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#"><i
+                            <div class="dropdown-menu__show">
+                            <li><a class="dropdown-item" href="#"><i
                                             class="fa-solid fa-house nav-items__icon__position"></i>Stays</a>
                                 </li>
                                 <li><a class="dropdown-item" href="#"><i
@@ -33,6 +34,7 @@
                                 </li>
                                 <li><a class="dropdown-item" href=""><i
                                             class="fa-solid fa-car-side nav-items__icon__position"></i>Cars</a></li>
+                            </div>
                             </ul>
                         </div>
 
@@ -45,18 +47,29 @@
                     <a class="navbar-button" href="">
                         List your property
                     </a>
-                    <div class="navbar-notification">
+                    <!-- <div class="navbar-notification">
                         <div class="navbar-icon__center">
                             <i class="fa-regular fa-bell" style="font-size: 20px"></i>
                         </div>
+                    </div> -->
+                    <div class="btn-group">
+                        <button class="btn btn-secondary" type="" id="dropdownMenuButton"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="fa-regular fa-bell" style="font-size: 20px"></i>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <li><a class="dropdown-item" href="#">Menu item</a></li>
+                            <li><a class="dropdown-item" href="#">Menu item</a></li>
+                            <li><a class="dropdown-item" href="#">Menu item</a></li>
+                        </ul>
                     </div>
                     <div class="navbar-profile">
                         <div class="navbar-icon__center">
                             <a href="{{ route('signup') }}"><i class="fa-solid fa-circle-user"></i></a>
                             <div class="navbar-profile-card">
                                 <div class="btn-group">
-                                    <button class="btn dropdown-toggle" type="button"
-                                        data-bs-toggle="dropdown" aria-expanded="false">
+                                    <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                                        aria-expanded="false">
                                         Large button
                                     </button>
                                     <ul class="dropdown-menu">
